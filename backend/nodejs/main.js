7152224678e0f9c7c934171d6485d22a6de5d8d7
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2023-11-10 19:48:58 +0700

    add an URL route

--- backend/nodejs/main.js
+++ backend/nodejs/main.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const body_parser = require('body-parser')
 const userRoute = require('./routers/userRoute')
+const caloriesRoute = require('./routers/caloriesRoute')
+const foodRoute = require('./routers/foodRoute')
 
 const app = express()
 
 app.use(body_parser.json())
 app.use('/api/user', userRoute)
+app.use('/api/calories', caloriesRoute)
+app.use('/apu/food', foodRoute)
 
 app.listen(14000, () => {
   console.log("The server is running on port 14000")
