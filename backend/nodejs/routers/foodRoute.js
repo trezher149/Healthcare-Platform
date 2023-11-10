8fdab33023940237df343bea27a0d7542aad7d62
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2023-11-10 19:45:28 +0700

    add foodRoute

---
+++ backend/nodejs/routers/foodRoute.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const { getFoodList, getAllFoodList, addFood } = require('../controllers/foodListController')
+
+const mongodbName = process.env.MONGODB_ADMINUSERNAME
+const mongodbPasswd = process.env.MONGODB_ADMINPASSWD
+const dbName = process.env.MONGODB_NAME
+
+router.post('/getFoodList', (req, res) => {
+  mongoose.connect(`mongodb://${mongodbName}:${mongodbPasswd}@${dbName}:27017/`)
+  getFoodList(req.body.calories, req.body.foodIds)
+  .then((result) => {
+    res.json({"foodList": result})
+  })
+  .catch((error) => {
+    console.log(error)
+    res.status(500).send("Error!")
+  })
+})
