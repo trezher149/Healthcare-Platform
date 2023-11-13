1a2e4c21d22fe12cbaaa2edc4b5e44395e67e69b
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2023-11-13 16:13:15 +0700

    add image saving fuctionality

--- backend/linebotImg/main.py
+++ backend/linebotImg/main.py
@@ -1,4 +1,17 @@
+# Original code by Yang-33 (https://github.com/line/line-bot-sdk-python/blob/master/examples/flask-echo/app.py#L51)
+# Edit and modified by trezher149 (Thunyavat Thanakornsombat)
+
+
 from flask import Flask, request, abort
+from logging.config import dictConfig
+import ngrok, os, time, requests, io, base64
+from dotenv import load_dotenv
+import PIL.Image as Image
+
+load_dotenv()
+
+CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET")
 
 from linebot.v3 import (
     WebhookHandler
@@ -11,18 +24,30 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
+    ImageMessage,
 )
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent
+    TextMessageContent,
+    ImageMessageContent
 )
+
+dictConfig({
+    'root':{
+        "level": 'INFO'
+    },
+    "version": 1
+})
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
+@app.route("/test", methods=["GET"])
+def home():
+    return "Hello world"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -54,5 +79,22 @@ def handle_message(event):
             )
         )
 
+@handler.add(MessageEvent, message=ImageMessageContent)
+def handle_image(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        picture_id = event.message.id
+        headers = {'Authorization': 'Bearer {' + f'{CHANNEL_ACCESS_TOKEN}' + '}'}
+        binary = requests.get(f'https://api-data.line.me/v2/bot/message/{picture_id}/content', headers=headers)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text="Picture Send!")]
+            )
+        )
+        img = Image.open(io.BytesIO(binary.content))
+        img.save(f"./pictures/{picture_id}.jpg")
+
 if __name__ == "__main__":
-    app.run()
+    ngrok.connect(12777, authtoken_from_env=True)
+    app.run(port=12777)
