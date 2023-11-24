54e5785abb221c32617c5854eb513eae51db7928
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2023-11-24 10:34:49 +0700

    Line bot can use backend API to add line user to website user and get picture from Line to calculate score (Calories)

--- backend/linebotImg/main.py
+++ backend/linebotImg/main.py
@@ -7,11 +7,18 @@ from logging.config import dictConfig
 import ngrok, os, time, requests, io, base64
 from dotenv import load_dotenv
 import PIL.Image as Image
+import usermodules as ump
+import random as rand
 
 load_dotenv()
 
 CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET")
+
+URL = "http://cheffy:14000/api"
+USER_URL = URL + "/user"
+CAL_URL = URL + "/calories"
+SLEEP_URL = URL + "/sleep"
 
 from linebot.v3 import (
     WebhookHandler
@@ -30,7 +37,7 @@ from linebot.v3.messaging import (
 from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent,
-    ImageMessageContent
+    ImageMessageContent,
 )
 
 dictConfig({
@@ -72,12 +79,16 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+        if (event.message.text[0] == 'i'):
+            # if ump.add_user_id(USER_URL + "/addlineiduser", event.source.to_dict()['userId'], event.message.text[1:]):
+            if ump.add_user_id(USER_URL + "/addlineiduser", event.source.to_dict()['userId'], '375d99cbcf2a'):
+                send_msg(line_bot_api, event, "ผูกบัญชี line กับบัญชีเรียบร้อยแล้วค่า~")
+            else:
+                send_msg(line_bot_api, event, "คุณได้ผูก line กับบัญชีของคุณแล้วนะคะ")
+            return
+        app.logger.info(event.source.to_dict()['userId'])
+        send_msg(line_bot_api, event, "หนูไม่รู้คำสั่งนี้นะคะ")
+                
 
 @handler.add(MessageEvent, message=ImageMessageContent)
 def handle_image(event):
@@ -86,14 +97,31 @@ def handle_image(event):
         picture_id = event.message.id
         headers = {'Authorization': 'Bearer {' + f'{CHANNEL_ACCESS_TOKEN}' + '}'}
         binary = requests.get(f'https://api-data.line.me/v2/bot/message/{picture_id}/content', headers=headers)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text="Picture Send!")]
-            )
-        )
         img = Image.open(io.BytesIO(binary.content))
         img.save(f"./pictures/{picture_id}.jpg")
+        status = ump.send_calories(URL, event.source.to_dict()['userId'], rand.randint(600, 1500))
+        if status == "400":
+            send_msg(line_bot_api, event, "คุณยังไม่ได้ผูกบัญชีกับบัญชี line ของคุณนะคะ")
+        else:
+            send_msg(line_bot_api, event, f"คุณได้ {str(status)} คะแนนค่า")
+
+def send_msg(api, event, msg, emojis = []):
+    if len(emojis) == 0:
+        api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=msg)]
+            )
+        )
+    else:
+        api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=msg, emojis=emojis)]
+            )
+        )
+
+
 
 if __name__ == "__main__":
     ngrok.connect(12777, authtoken_from_env=True)
---
+++ backend/linebotImg/usermodules.py
@@ -0,0 +1,15 @@
+import requests
+
+def add_user_id(url, lineid, userid) -> bool:
+  res = requests.post(url, json={"userId": userid, "lineId": lineid})
+  if res.status_code == 200:
+    return True
+  return False
+
+def send_calories(url, lineid, calories):
+  res = requests.post(url + "/user/getuseridfromlineid", json={"lineId": lineid})
+  if res.status_code == 400:
+    return str(res.status_code)
+  data = res.json()
+  res = requests.post(url + "/calories/updateCal", json={"userId": data["userId"], "calories": calories})
+  return res.json()["score"]
