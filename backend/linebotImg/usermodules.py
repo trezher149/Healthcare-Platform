cea96aa901b2b7b6aed318ed7de24c3f6c7e186e
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2024-01-30 16:04:04 +0700

    Add image manipulation functionalities

--- backend/linebotImg/usermodules.py
+++ backend/linebotImg/usermodules.py
@@ -6,6 +6,14 @@ def add_user_id(url, lineid, userid) -> bool:
     return True
   return False
 
+def get_user_id(url, lineid):
+  res = requests.post(url + "/user/getuseridfromlineid", json={"lineId": lineid})
+  if res.status_code == 200:
+    return res.json()["userId"]
+  else:
+    return 400
+
+
 def send_calories(url, lineid, calories):
   res = requests.post(url + "/user/getuseridfromlineid", json={"lineId": lineid})
   if res.status_code == 400:
