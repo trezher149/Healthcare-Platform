39a3c66423370758a8f4b936245731b53d067e4e
Thunyavat Thanakornsombat <thunyavat.ComEngr@hotmail.com>
2024-04-04 11:38:22 +0700

    implement JWT

--- backend/nodejs/main.js
+++ backend/nodejs/main.js
@@ -15,6 +15,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(body_parser.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors({
   origin: [`${frontendURL}:${frontendPort}`]
 }))
--- backend/nodejs/routers/caloriesRoute.js
+++ backend/nodejs/routers/caloriesRoute.js
@@ -3,29 +3,55 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const {updateCalories, getCaloriesSeriesData} = require('../controllers/seriesData/caloriesController')
 const {setCaloriesGoal} = require('../controllers/staticData/caloriesController')
+const tokenManager = require('./tokenManager')
 
 const mongodbName = process.env.MONGODB_ADMINUSERNAME
 const mongodbPasswd = process.env.MONGODB_ADMINPASSWD
-const dbName = process.env.MONGODB_NAME
+const mongoServerName = process.env.MONGODB_NAME
+const mongodbPort = process.env.MONGODB_PORT
+
+const mongodbURI = process.env.MONGODB_ATLAS_NAME ? process.env.MONGODB_ATLAS_NAME : 
+                    `mongodb://${mongodbName}:${mongodbPasswd}@${mongoServerName}:${mongodbPort}/`
 
 router.post('/updateCal', (req, res) => {
-  mongoose.connect(`mongodb://${mongodbName}:${mongodbPasswd}@${dbName}:27017/`)
-  updateCalories(req.body.userId, req.body.calories)
-  .then(scoreData => res.json(scoreData))
+  const decoded = tokenManager.headerTokenDecode("linebot-public.pem", req.headers.authorization)
+  mongoose.connect(mongodbURI)
+  updateCalories(decoded.userId, req.body.calories)
+  .then((scoreData) => {
+    const encoded = tokenManager.generateToken("backend-private.pem", scoreData)
+    console.log(encoded)
+    res.json({payload: encoded})
+  })
   .catch(errCode => res.sendStatus(errCode))
 })
 
 router.post('/getCal', (req, res) => {
-  mongoose.connect(`mongodb://${mongodbName}:${mongodbPasswd}@${dbName}:27017/`)
-  getCaloriesSeriesData(req.body.userId, req.body.lenghtDays)
-  .then(data => res.json(data))
+  var fromLineBot = true
+  const auth_header = req.headers.authorization
+  try {
+    var decoded = tokenManager.headerTokenDecode("linebot-public.pem", auth_header)
+    console.log("nice")
+  } catch(err) {
+    var decoded = tokenManager.headerTokenDecode("web-public.pem", auth_header)
+    fromLineBot = false
+  }
+  console.log(decoded)
+  mongoose.connect(mongodbURI)
+  getCaloriesSeriesData(decoded.userId, req.body.lenghtDays)
+  .then((data) => {
+    const privt_key = fromLineBot ? "backend-private.pem" : "b2w-private.pem"
+    console.log({data})
+    const encoded = tokenManager.generateToken(privt_key, {data})
+    res.send({payload: encoded})
+  })
   .catch(errCode => res.sendStatus(errCode))
 })
 
 router.post('/setCalGoal', (req, res) => {
-  mongoose.connect(`mongodb://${mongodbName}:${mongodbPasswd}@${dbName}:27017/`)
-  setCaloriesGoal(req.body.userId, req.body.caloriesGoal, req.body.endDays)
-  .then((obj) => {res.json(obj)})
+  const decoded = tokenManager.headerTokenDecode("linebot-public.pem", req.headers.authorization)
+  mongoose.connect(mongodbURI)
+  setCaloriesGoal(decoded.userId, req.body.caloriesGoal, req.body.endDays)
+  .then(goalData => res.json(goalData))
   .catch((status) => {res.sendStatus(status)})
 })
 
